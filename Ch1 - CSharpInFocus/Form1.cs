3554afb998276c62880539a522c3734bf5dd736e
Dirk Strauss <dirk.strauss@thisisembrace.com>
2018-12-29 22:27:05 +0200

    Added Tuple Example and Pattern Matching Example code.

--- Ch1 - CSharpInFocus/Form1.cs
+++ Ch1 - CSharpInFocus/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,82 @@ namespace CSharpInFocus
 {
     public partial class Form1 : Form
     {
+        public enum InstrumentType { guitar, cello, violin }
         public Form1()
         {
             InitializeComponent();
         }
+                
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            #region Tuples
+            TestTuple();
+            InstanceTupleVariables();
+            ComparingTuples(); 
+            #endregion
+        }
+
+        #region Tuple Examples
+        private void TestTuple()
+        {
+            TupleExample te = new TupleExample();
+            //(string BrandType, int GuitarStringCount) = te.GetGuitarType();
+            //var (BrandType, GuitarStringCount) = te.GetGuitarType();
+            (string BrandType, var GuitarStringCount) = te.GetGuitarType();
+
+            Debug.WriteLine(BrandType);
+            Debug.WriteLine(GuitarStringCount);
+
+
+        }
+
+        private void InstanceTupleVariables()
+        {
+            string instrumentType = nameof(InstrumentType.guitar);
+            int strings = 12;
+            (string TypeOfInstrument, int NumberOfStrings) instrument = (instrumentType, strings);
+            PlayInstrument(instrument);
+        }
+
+        private void PlayInstrument((string instrument, int strings) instrumentToPlay)
+        {
+            Debug.WriteLine($"I am playing a {instrumentToPlay.instrument} with {instrumentToPlay.strings} strings");
+        }
+
+        private void ComparingTuples()
+        {
+            string instrumentType1 = nameof(InstrumentType.guitar);
+            int stringsCount1 = 6;
+            (string TypeOfInstrument, int NumberOfStrings) instrument1 = (instrumentType1, stringsCount1);
+
+            string instrumentType2 = nameof(InstrumentType.violin);
+            int stringsCount2 = 4;
+            (string TypeOfInstrument, int NumberOfStrings) instrument2 = (instrumentType2, stringsCount2);
+
+            if (instrument1.NumberOfStrings != instrument2.NumberOfStrings)
+            {
+                Debug.WriteLine($"A {instrument2.TypeOfInstrument} does not have the same number of strings as a {instrument1.TypeOfInstrument}");
+            }
+
+            if (!instrument1.Equals(instrument2))
+            {
+                Debug.WriteLine("We are dealing with different instruments here.");
+            }
+
+
+            if (instrument1 != instrument2)
+            {
+
+            }
+
+        }
+
+        private void InferredTupleElementNames()
+        {
+            string instrumentType = nameof(InstrumentType.guitar);
+            int stringsCount = 6;
+            var instrument = (instrumentType, stringsCount);
+        } 
+        #endregion
     }
 }
---
+++ Ch1 - CSharpInFocus/PatternMatchingExample.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpInFocus
+{
+    public enum UniversityCourses { Maths, Chemistry, Anatomy, LifeSkills }
+    public enum UniversityDegree { BA, BSc }
+    public enum Countries { USA, UK, Germany, Australia }
+    public class PatternMatchingExample
+    {
+        
+        public string GetPersonCourse(object someperson)
+        {
+            //// Before C# 7
+            //if (someperson is Student)
+            //{
+            //    var student = (Student)someperson;
+            //    var details = student.StudentDetails();
+            //    return details.studentCourse;
+            //}
+            //else if (someperson is Lecturer)
+            //{
+            //    var lecturer = (Lecturer)someperson;
+            //    return lecturer.CourseSpecialization;
+            //}
+            //else if (someperson is Alumnus)
+            //{
+            //    var alumnus = (Alumnus)someperson;
+            //    return alumnus.DegreeObtained;
+            //}
+
+            //// The is type pattern
+            //if (someperson is Student student)
+            //{
+            //    return student.StudentDetails().studentCourse;
+            //}
+            //else if (someperson is Lecturer lecturer)
+            //{
+            //    return lecturer.CourseSpecialization;
+            //}
+            //else if (someperson is Alumnus alumnus)
+            //{
+            //    return alumnus.DegreeObtained;
+            //}
+            //else if (someperson is ExchangeStudent exchStudent)
+            //{
+            //    return exchStudent.ShortCourse;
+            //}
+
+
+
+            // Using switch statements pattern matching
+            switch (someperson)
+            {
+                case Student student:
+                    return student.CourseEnrolledFor;
+                case Lecturer lecturer:
+                    return lecturer.CourseSpecialization;
+                case Alumnus alumnus:
+                    return alumnus.DegreeObtained;
+                case ExchangeStudent exchangeStudent:
+                    return exchangeStudent.ShortCourse;
+                default:
+                    return "No course determined";
+            }
+        }
+
+        
+
+    }
+
+
+    public class Student : Person
+    {
+        public int StudentNumber { get; }
+        public string CourseEnrolledFor { get; }
+        public Student((string firstname, string lastname, int age) personDetails, int studentNumber, UniversityCourses courseEnrolled)
+        {
+            FirstName = personDetails.firstname;
+            LastName = personDetails.lastname;
+            Age = personDetails.age;
+            StudentNumber = studentNumber;
+            CourseEnrolledFor = nameof(courseEnrolled);
+        }
+
+        public (string firstname, string lastname, int studentNum, string studentCourse) StudentDetails()
+        {
+            var studentDetails = (FirstName, LastName, StudentNumber, CourseEnrolledFor);
+            return studentDetails;
+        }
+    }
+
+    public class Lecturer : Person
+    {
+        public int EmployeeNumber { get; }
+        public string CourseSpecialization { get; }
+        public int YearEmployed { get; }
+        public Lecturer((string firstname, string lastname, int age) personDetails, int employeeNumber, UniversityCourses courseSpecialization, int yearFirstEmployed)
+        {
+            FirstName = personDetails.firstname;
+            LastName = personDetails.lastname;
+            Age = personDetails.age;
+            EmployeeNumber = employeeNumber;
+            CourseSpecialization = nameof(courseSpecialization);
+            YearEmployed = yearFirstEmployed;
+        }
+
+        public (string, string, int, string, int) LecturerDetails()
+        {
+            var lecturerDetails = (FirstName, LastName, EmployeeNumber, CourseSpecialization, YearEmployed);
+            return lecturerDetails;
+        }
+    }
+
+    public class Alumnus : Person
+    {
+        public int YearCompleted { get; }
+        public string DegreeObtained { get; }
+        public Alumnus((string firstname, string lastname, int age) personDetails, int yearStudiesCompleted, UniversityDegree degreeObtained)
+        {
+            FirstName = personDetails.firstname;
+            LastName = personDetails.lastname;
+            Age = personDetails.age;
+            YearCompleted = yearStudiesCompleted;
+            DegreeObtained = nameof(degreeObtained);
+        }
+
+        public (string, string, int, string) AlumnusDetails()
+        {
+            var alumnusDetails = (FirstName, LastName, YearCompleted, DegreeObtained);
+            return alumnusDetails;
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }               
+    }
+        
+    public struct ExchangeStudent
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string ShortCourse { get; }
+        public int VisaExpiryDate { get; }
+
+        public ExchangeStudent((string firstname, string lastname, int age) personDetails, UniversityCourses shortCourse, int studentVisaExpiry)
+        {
+            FirstName = personDetails.firstname;
+            LastName = personDetails.lastname;
+            ShortCourse = nameof(shortCourse);
+            VisaExpiryDate = studentVisaExpiry;
+        }
+
+        public (string, string, string, int) ExchangeStudentDetails()
+        {
+            var exchangeDetails = (FirstName, LastName, ShortCourse, VisaExpiryDate);
+            return exchangeDetails;
+        }
+    }
+}
