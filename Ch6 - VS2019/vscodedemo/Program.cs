1cf95d313dee5af5f6fb7dbb1b6c73a4fec4a24e
Dirk Strauss <dirk.strauss@thisisembrace.com>
2019-02-22 09:10:54 +0200

    Code samples for chapter 6 on Visual Studio 2019. Updated README.md file.

---
+++ Ch6 - VS2019/vscodedemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace vscodedemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var lstWidgets = 
+            //    new List<string>()
+            //    { "widget1", "widget2", "widget3" };
+
+            //foreach (var widget in lstWidgets
+            //    .Where(widget => widget.EndsWith("2"))
+            //    .Select(widget => widget))
+            //{
+            //    WriteLine($"{widget} exists in the list");
+            //}
+
+
+            //// Example of using let
+            //var lstStockCode =
+            //    new List<string>()
+            //    { "A100-341", "B101-754", "A100-197", "C201-341", "B102-774", "C101-111", "A100-774", "C105-191" };
+
+            //var classAStockCodes =
+            //    from aclass in lstStockCode
+            //    let a = (aclass.StartsWith("A100") ? (aclass.Replace("A100-", "")) : "0")
+            //    where Convert.ToInt32(a) > 200 
+            //    && Convert.ToInt32(a) > 0
+            //    select aclass;
+
+            //foreach(var cl in classAStockCodes)
+            //{
+            //    WriteLine($"{cl} is a class A stock code in the 200 plus range");
+            //}
+
+            string FirstName = "Dirk";
+            string LastName = "Strauss";
+            string FullName = string.Format("My name is {0} {1}", FirstName, LastName);
+
+            ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -109,3 +109,8 @@ Microsoft has done a huge amount of work to bring Visual Studio to all platforms
 
 ## Debug your ASP.NET Core MVC project with Visual Studio Code
 Visual Studio Code allows you to debug your code. You need to do a little heavy lifting to get this all set up, but once you have done this, you're all sorted.
+
+# Chapter 6 - Being more productive in Visual Studio 2019
+
+## Visual Studio Code Tips
+In this section of the book we will be looking at some general Visual Studio tips. You can use these in Visual Studio to improve your code and become more productive in your day-to-day coding.
